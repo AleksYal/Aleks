8fb72cb81391c43aedb89188f17a1280b8da8ee1
AleksYal <yalyshev1993@gmail.com>
2025-04-16 14:58:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-// main Nightscout server file
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send('Nightscout server is running!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
